a2cd983052761778f6ec96c699320a1982aaaca3
Pavani-7036 <pavanijambuluripavani@gmail.com>
2025-06-11 16:20:47 +0530

    Add files via upload

---
+++ HeapSort.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+class HeapSort {
+    static void heapify(int arr[], int n, int i) {
+        int largest = i; 
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        if (l < n && arr[l] > arr[largest]) {
+            largest = l;
+        }
+        if (r < n && arr[r] > arr[largest]) {
+            largest = r;
+        }
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, n, largest);
+        }
+    }
+
+    static void heapSort(int arr[]) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+    static void printArray(int arr[]) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {9, 4, 8, 3, 2, 6, 1};
+        System.out.println("Original array:");
+        printArray(arr);
+        heapSort(arr);
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
